055bc26ffd6ecdbaba3f042d9e48ced573ea73b9
bolishettianjali <bolishettianjali@gmail.com>
2021-11-20 18:42:51 +0530

    Added: add method to create Node object and add the data to created Node objects

--- src/com/linkedList/SimpleLinkedList.java
+++ src/com/linkedList/SimpleLinkedList.java
@@ -8,4 +8,30 @@ public class SimpleLinkedList {
      */
     Node head, tail;
 
+    /**
+     * Method is to create, initialize and Link the new Node
+     * object with previous Node object.
+     */
+    public void add(int data) {
+        Node node = new Node(data);//50
+
+        if (head == null) {
+            head = node;
+        } else {
+            tail.nextNode = node;
+        }
+        tail = node;
+    }
+
+    /**
+     * Method to display all the data in Node objects
+     */
+    public void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.nextNode;
+        }
+    }
 }
--- src/com/linkedList/TestSimpleLinkedList.java
+++ src/com/linkedList/TestSimpleLinkedList.java
@@ -4,5 +4,9 @@ public class TestSimpleLinkedList {
 
     public static void main(String[] args) {
         SimpleLinkedList list = new SimpleLinkedList();
+        list.add(70);
+        list.add(30);
+        list.add(56);
+        list.display();
     }
 }
